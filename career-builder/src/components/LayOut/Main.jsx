306cfa7ef6aa11f3e229b7d7939d2e2637b9489b
Mohosin <mohosinali075@gmail.com>
2023-04-10 22:05:02 +0600

    added some components && design navbar with react route

---
+++ career-builder/src/components/LayOut/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
+
+const Main = () => {
+    return (
+        <div className='max-w-[1920px] mx-auto px-1 2xl:px-72 xl:px-20'>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
--- career-builder/src/main.jsx
+++ career-builder/src/main.jsx
@@ -2,9 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/LayOut/Main';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Blogs from './components/Blogs/Blogs';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children : [
+      {
+        path : '/',
+        element : <Home></Home>
+      },
+      {
+        path : '/statistics',
+        element : <Statistics></Statistics>
+      },
+      {
+        path : '/applied',
+        element : <AppliedJobs></AppliedJobs>
+      },
+      {
+        path : '/blog',
+        element : <Blogs></Blogs>
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
